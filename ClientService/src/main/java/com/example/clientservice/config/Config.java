4401554dcb5c1aa454b2cd1c47e0a90ad9a29c99
hagar <hagarallam.dev@gmail.com>
2024-02-24 13:50:49 +0200

    set timeout for rest template builder

--- ClientService/src/main/java/com/example/clientservice/config/Config.java
+++ ClientService/src/main/java/com/example/clientservice/config/Config.java
@@ -5,11 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class Config {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){
-        return builder.build();
+        return builder
+                .setReadTimeout(Duration.ofMillis(1000))
+                .build();
     }
 }
